6fc423b61aa379c03075d0a6e53f21550bf8f3ab
Rerigan <cosnnum@gmail.com>
2025-05-10 17:40:08 -0300

    some changes, need to fix usestate

--- src/Depoimentos.tsx
+++ src/Depoimentos.tsx
@@ -5,7 +5,7 @@ function Depoimentos() {
         id="contact-section"
         className="section relative min-h-[40rem] w-full overflow-hidden bg-gray-100 "
       >
-        <div className="text-center mt-40">
+        <div className="text-center mt-40 px-6">
           <h2 className="font-bold text-xl mb-10">Te ajudamos a encontrar o candidato perfeito!</h2><h1 className="text-xl font-bold p-4 inline-flex bg-blue-600 text-white rounded">Entre em contato!</h1></div>
           <p className="text-[15px] text-center absolute bottom-5 block left-[50%] translate-x-[-50%]">©Âncora RH, 2025</p>
         
--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,12 +1,25 @@
 import { Anchor } from 'lucide-react'
+import { Menu } from 'lucide-react';
+import { X } from 'lucide-react';
 const NavBar = () => {
+  function openNav() {
+    const linksBar  = document.getElementById('linksbar');
+    linksBar?.classList.toggle('opacity-100')
+    linksBar?.classList.toggle('flex-col')
+    linksBar?.classList.toggle('flex')
+    linksBar?.classList.toggle('text-center')
+    linksBar?.classList.toggle('mt-20')
+    document.getElementById("x")?.classList.toggle('hidden')
+    document.getElementById("menu")?.classList.toggle('hidden')
+  }
   return (
-    <div id="navbar" className="w-full fixed top-0 pt-8 justify-center gap-10 font-bold text-blue-800 z-2 pb-2">
+    <div id="navbar" className="w-full fixed top-0 pt-8 justify-center gap-10 font-bold text-blue-800 z-2 pb-2 select-none">
+      <div id="menu-open" className="md:hidden flex fixed left-8"><a onClick={(openNav)}><Menu id="menu"/> <X id="x" className="hidden"/></a></div>
         <div id="logo" className="md:absolute md:left-5 flex items-center justify-center bg-blue top-6">
           <Anchor className="mr-2 " size={35} />
           <p className="titletext text-2xl ml-2">Âncora RH</p>
         </div>
-        <div className="justify-center md:flex gap-20 hidden ">
+        <div id="linksbar" className=" justify-center md:flex gap-20 opacity-0 md:opacity-100 ">
         
         <a href="#hero-section">Início</a>
         <a href="#servicos-section">Serviços</a>
